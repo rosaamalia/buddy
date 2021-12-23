a6c28822af418abc9f7fb87bf7fb2649661c98fe
Shinjixy <singgih.uhamka@gmail.com>
2021-12-23 16:57:19 +0700

    Selesai Membuat CRUD User

--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const UserController = require('../controllers/UserController')
 
 router.get('/', UserController.getAllUser);
+router.post('/', UserController.createUser);
+router.patch('/:id', UserController.updateUser);
+router.delete('/:id', UserController.deleteUser);
 
 module.exports = router;
